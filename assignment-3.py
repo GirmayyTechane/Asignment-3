23b4424851f1f844a282ff1a1d7e690e4554347b
GirmayyTechane <43317998+GirmayyTechane@users.noreply.github.com>
2018-10-31 06:50:01 +0100

    q& a codes asgnmt-3

--- assignment-3.py
+++ assignment-3.py
@@ -23,6 +23,19 @@ lst =range(20)
 
 list(filter(odd_check,lst))
 
+# 2 Implement List comprehensions to produce the following lists.
+Write List comprehensions to produce the following Lists
+['A', 'C', 'A', 'D', 'G', 'I', ’L’, ‘ D’]
+['x', 'xx', 'xxx', 'xxxx', 'y', 'yy', 'yyy', 'yyyy', 'z', 'zz', 'zzz', 'zzzz']
+['x', 'y', 'z', 'xx', 'yy', 'zz', 'xxx', 'yyy', 'zzz', 'xxxx', 'yyyy', 'zzzz']
+[[2], [3], [4], [3], [4], [5], [4], [5], [6]]
+[[2, 3, 4, 5], [3, 4, 5, 6], [4, 5, 6, 7], [5, 6, 7, 8]]
+[(1, 1), (2, 1), (3, 1), (1, 2), (2, 2), (3, 2), (1, 3), (2, 3), (3, 3)]
+
+mystring = "ACADGILD"
+mystr_lst = [ strings for strings in mystring ]
+print ("ACADGILD == " + str(mystr_lst))
+
 
 #3. Implement a function longestWord() that takes a list of words and returns the longest one
 
