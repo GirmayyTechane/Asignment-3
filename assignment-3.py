1cbe1cdd6fe9162a7a846d7bef62cf485baa7d49
GirmayyTechane <43317998+GirmayyTechane@users.noreply.github.com>
2018-10-31 06:53:09 +0100

    Update assignment-3.py

--- assignment-3.py
+++ assignment-3.py
@@ -32,10 +32,40 @@ Write List comprehensions to produce the following Lists
 [[2, 3, 4, 5], [3, 4, 5, 6], [4, 5, 6, 7], [5, 6, 7, 8]]
 [(1, 1), (2, 1), (3, 1), (1, 2), (2, 2), (3, 2), (1, 3), (2, 3), (3, 3)]
 
+#wrtie list comprehensions to produce -> ['A', 'C', 'A', 'D', 'G', 'I', 'L', 'D']
 mystring = "ACADGILD"
 mystr_lst = [ strings for strings in mystring ]
 print ("ACADGILD == " + str(mystr_lst))
 
+#wrtie list comprehensions to produce -> ['x', 'xx', 'xxx', 'xxxx', 'y', 'yy', 'yyy', 'yyyy', 'z', 'zz', 'zzz', 'zzzz']
+myinput_list = ['x','y','z']# input string
+mynew_lst = [ i*n for i in myinput_list for n in range(1,5)]# comprehension list of lists
+print("my new list --> " +   str(mynew_lst)) # print result
+
+#wrtie list comprehensions to produce -> ['x', 'y', 'z', 'xx', 'yy', 'zz', 'xxx', 'yyy', 'zzz', 'xxxx', 'yyyy', 'zzzz']
+myinput_list = ['x','y','z']# input string
+mynew_lst = [ i*n for n in range(1,5) for i in myinput_list  ]# comprehension list of lists
+print("my new list --> " +   str(mynew_lst))# print result
+
+#wrtie list comprehensions to produce ->[[2], [3], [4], [3], [4], [5], [4], [5], [6]]
+
+myinput_list = [2,3,4]# input numbers
+mynew_lst = [ [i+n] for i in myinput_list for n in range(4)]# comprehension list of lists
+print("my new list --> " +   str(mynew_lst))# print result
+
+#wrtie list comprehensions to produce ->[[2, 3, 4, 5], [3, 4, 5, 6], [4, 5, 6, 7], [5, 6, 7, 8]]
+
+
+myinput_list = [2,3,4,5]# input numbers
+mynew_lst = [ [i+n for i in range(4)] for n in myinput_list]# comprehension list of lists
+print("my new list --> " +   str(mynew_lst))# print result
+
+#wrtie list comprehensions to produce ->[(1, 1), (2, 1), (3, 1), (1, 2), (2, 2), (3, 2), (1, 3), (2, 3), (3, 3)]
+
+
+myinput_list = [1,2,3]# input numbers
+mynew_lst = [(n,i) for i in range(1,4) for n in myinput_list]# comprehension list of lists
+print("my new list --> " +   str(mynew_lst))# print result
 
 #3. Implement a function longestWord() that takes a list of words and returns the longest one
 
